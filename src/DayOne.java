ebe2ca294f723175f957b291ff3860e9c2a650ae
St12n <stijn.schouppe@hotmail.com>
2022-09-14 17:04:58 +0200

    First part of the first advent of code exercise - got the path - got arraylist - got answer

--- src/DayOne.java
+++ src/DayOne.java
@@ -2,15 +2,41 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayOne {
 
     public static void main(String[] args) {
-        Path path = Paths.get("C:/_Projects/advent-of-code-2021/src/inputAdventOfCode1.txt");
+
+        List<Integer> dataInput = new ArrayList<>();
         try {
-            int lengte = Files.lines(path).toArray().length;
+            var lines = Files.readAllLines(Path.of("C:/_Projects/advent-of-code-2021/src/inputAdventOfCode1.txt"));
+            for (var line : lines) {
+                dataInput.add(Integer.parseInt(line));
+            }
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println(ex);
         }
+        int amountIncreased = 0;
+
+        for (int i = 0; i < dataInput.size() -1; i++){
+            if (dataInput.get(i) < dataInput.get(i+1)) {
+                amountIncreased++;
+            }
+        }
+        System.out.println(amountIncreased);
     }
+
+
+
+
+//        Path path = Paths.get("C:/_Projects/advent-of-code-2021/src/inputAdventOfCode1.txt");
+//        try {
+//            int lengte = Files.lines(path).toArray().length;
+//            System.out.println(lengte);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 }
